eb97ac55b1b3f51777f948b65fd3e2de2d6926ec
Rotem Tamir <rotemtamir@gmail.com>
2023-02-01 15:30:44 +0200

    main: extract input

--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 
+	"ariga.io/atlas-sync-action/internal/atlascloud"
+	"github.com/sethvargo/go-githubactions"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,4 +22,29 @@ h1:X97sBPjOeiRWeoEdqpIpHAdzlshqOqllEKrJS9JruPo=
 20230201094614.sql h1:GkisBcewe1zpcP5IGGkbbsYSKJgV+fzlDVal+FKEjoE=
 `
 	require.Equal(t, exp, arc)
+}
+
+func TestInput(t *testing.T) {
+	env := map[string]string{
+		"GITHUB_REPOSITORY": "ariga/test",
+		"GITHUB_SHA":        "1234567890",
+		"INPUT_DIR":         "migrations/",
+		"INPUT_DRIVER":      "mysql",
+		"GITHUB_REF_NAME":   "master",
+		"GITHUB_EVENT_PATH": "internal/testdata/push_event.json",
+	}
+	act := githubactions.New(githubactions.WithGetenv(func(key string) string {
+		return env[key]
+	}))
+	input, err := Input(act)
+	require.NoError(t, err)
+	require.EqualValues(t, atlascloud.UploadDirInput{
+		Repo:      "ariga/test",
+		Commit:    "1234567890",
+		Branch:    "master",
+		Path:      "migrations/",
+		Driver:    atlascloud.DriverMysql,
+		Url:       "https://github.com/ariga/atlas-sync-action/commit/4a3f0bcb6dff19078393728f1b69d89d853771eb",
+		DirFormat: atlascloud.DirFormatAtlas,
+	}, input)
 }
