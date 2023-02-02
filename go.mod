882bfe3c8db1722abf51d036c72b2203c056b3df
Rotem Tamir <rotemtamir@gmail.com>
2023-02-02 09:31:43 +0200

    main: extract mutation input from action env (#5)

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	ariga.io/atlas v0.9.1-0.20230130073334-c0b12de7df90
 	github.com/Khan/genqlient v0.5.0
+	github.com/mitchellh/mapstructure v1.5.0
 	github.com/sethvargo/go-githubactions v1.1.0
 	github.com/stretchr/testify v1.7.1-0.20210427113832-6241f9ab9942
 	golang.org/x/tools v0.5.0
@@ -12,6 +13,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sethvargo/go-envconfig v0.8.3 // indirect
 	github.com/vektah/gqlparser/v2 v2.4.5 // indirect
--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strings"
 
+	"ariga.io/atlas-sync-action/internal/atlascloud"
 	"ariga.io/atlas/sql/migrate"
+	"github.com/mitchellh/mapstructure"
 	"github.com/sethvargo/go-githubactions"
 	"golang.org/x/tools/txtar"
 )
 
 func main() {
-	githubactions.Noticef("Hello, GitHub Actions")
+	act := githubactions.New()
+	input, err := Input(act)
+	if err != nil {
+		act.Fatalf("failed to parse input: %v", err)
+	}
+	githubactions.Noticef("%v", input)
 }
 
 // Archive returns a txtar archive of the given migration directory.
@@ -55,4 +63,50 @@ func Archive(path string) (string, error) {
 		Data: wantS,
 	})
 	return string(txtar.Format(arc)), nil
+}
+
+func Input(act *githubactions.Action) (atlascloud.UploadDirInput, error) {
+	c, err := act.Context()
+	if err != nil {
+		return atlascloud.UploadDirInput{}, err
+	}
+	org, repo := c.Repo()
+	ev := PushEvent{}
+	if err := mapstructure.Decode(c.Event, &ev); err != nil {
+		return atlascloud.UploadDirInput{}, err
+	}
+	di := act.GetInput("driver")
+	drv, err := driver(di)
+	if err != nil {
+		return atlascloud.UploadDirInput{}, err
+	}
+	return atlascloud.UploadDirInput{
+		Repo:      fmt.Sprintf("%s/%s", org, repo),
+		Branch:    c.RefName,
+		Commit:    c.SHA,
+		Path:      act.GetInput("dir"),
+		Url:       ev.HeadCommit.URL,
+		Driver:    drv,
+		DirFormat: atlascloud.DirFormatAtlas,
+	}, nil
+}
+
+func driver(s string) (atlascloud.Driver, error) {
+	switch s := strings.ToLower(s); s {
+	case "postgres":
+		return atlascloud.DriverPostgresql, nil
+	case "mysql":
+		return atlascloud.DriverMysql, nil
+	case "sqlite":
+		return atlascloud.DriverSqlite, nil
+	default:
+		return "", fmt.Errorf("unknown driver %q", s)
+	}
+}
+
+type PushEvent struct {
+	HeadCommit struct {
+		URL string `mapstructure:"url"`
+	} `mapstructure:"head_commit"`
+	Ref string `mapstructure:"ref"`
 }
